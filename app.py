5ad8297c2e6d5211642231b3ea38595073f6110e
fanecovsf <gustavo_h07@hotmail.com>
2023-07-17 11:02:37 -0300

    Refactor: Adicionado diretório de models

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, Response
 from flask_sqlalchemy import SQLAlchemy
+from models.models import db, Estudante
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///estudantes.sqlite3'
-
-db = SQLAlchemy(app)
-
-class Estudante(db.Model):
-    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
-    nome = db.Column(db.String(150))
-    idade = db.Column(db.Integer)
-
-    def __init__(self, nome, idade):
-        self.nome = nome
-        self.idade = idade
 
 
 
 @app.route('/')
 def index():
     estudantes = Estudante.query.all()
+    result = [e.to_dict() for e in estudantes]
     return render_template('index.html', estudantes=estudantes)
 
 
@@ -55,6 +46,7 @@ def edit(id):
 
 
 if __name__ == '__main__':
-    with app.app_context():
+    db.init_app(app=app)
+    with app.test_request_context():
         db.create_all()
     app.run(debug=True)
